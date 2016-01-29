501efaf6926f0dd00605c4952a4f9c156cc71efc
samijon <maximoradi123456789@gmail.com>
2016-01-29 08:42:48 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,8 +220,8 @@ function create_config( )
     "invite",
     "all",
     "leave_ban"
-    sudo_users = {122997453},--Sudo users
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    },
+    sudo_users = {122997453,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
